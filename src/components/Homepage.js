303f19c78ab70a6d0fa8fdab59ad4de1b4eab31e
brandonngiam <brandonngiam@user.noreply.github.com>
2019-06-14 18:02:42 +0800

    Small edits to styling. Last commit before adding tabs

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,13 +6,6 @@ import HomepageInfo from "./HomepageInfo";
 function Homepage(props) {
   return (
     <div className="start-page" data-testid="homepage">
-      <div className="description">
-        <h1>CPF Simulator</h1>
-        <p>
-          This is a tool that will simulate your CPF account balances until the
-          age of 55.
-        </p>
-      </div>
       <div>
         <div className="user-input-container">
           <UserInputForm
@@ -26,6 +19,13 @@ function Homepage(props) {
             monthlySalary={props.monthlySalary}
             history={props.history}
           />
+        </div>
+        <div className="description">
+          <h1>CPF Simulator</h1>
+          <p>
+            This is a tool that will simulate your CPF account balances until
+            the age of 55.
+          </p>
         </div>
       </div>
       <HomepageInfo />
